d738e9596c6e2b5d5a5659f63776bcb098505b11
Valentin Zapata <svt@users-MacBook-Pro.local>
2021-02-20 23:31:36 -0500

    webview spike

--- lib/module/events/event_list_view.dart
+++ lib/module/events/event_list_view.dart
@@ -1,48 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:tutorial2/widget/drawer.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class EventsPage extends StatelessWidget {
   static const String routeName = '/events';
 
   @override
   Widget build(BuildContext context) {
-    Future<void> _showMyDialog() async {
-      return showDialog<void>(
-        context: context,
-        barrierDismissible: false, // user must tap button!
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('AlertDialog Title'),
-            content: SingleChildScrollView(
-              child: ListBody(
-                children: <Widget>[
-                  Text('This is a demo alert dialog.'),
-                  Text('Would you like to approve of this message?'),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: Text('Approve'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-
     return new Scaffold(
         appBar: AppBar(
           title: Text("Events"),
         ),
         drawer: AppDrawer(),
-        body: Center(
-            child: new ElevatedButton(
-          child: new Text("click"),
-          onPressed: _showMyDialog,
-        )));
+        body: WebView(initialUrl: 'https://flutter.io'));
   }
 }
